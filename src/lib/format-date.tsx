4e314da6b4e1adb95a29b86d0e70af485bbf2087
Camila <camilaferna140494@gmail.com>
2025-07-12 16:59:32 -0300

    feat: migrate dayjs setup to a new module and remove old format-date.tsx

--- src/lib/format-date.tsx
+++
@@ -1,8 +0,0 @@
-import dayjsLib from 'dayjs'
-import 'dayjs/locale/pt-br'
-import relativeTime from 'dayjs/plugin/relativeTime'
-
-dayjsLib.locale('pt-br')
-dayjsLib.extend(relativeTime)
-
-export const dayjs = dayjsLib
